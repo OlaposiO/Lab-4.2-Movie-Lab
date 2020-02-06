cba71e3a3a95d09bb2b6593d23b7a8f4426d2266
oomishope <olaposiomishope@wuickenloans.com>
2020-02-06 14:23:53 -0500

    MovieApp Lab4.2

---
+++ Lab 4.2 Movie App/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4._2_Movie_App
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public Movie(string title, string genre)
+        {
+            Title = title;
+            Genre = genre;
+        }
+
+
+        //private string title;
+        //private string genre;
+        //public string Title { get => title; set => title = value; }
+        //public string Genre { get => genre; set => genre = value; }
+
+        //public Movie(string title, string genre)
+        //{
+        //    Title = title;
+        //    Genre = genre;
+        //}
+    }
+}
---
+++ Lab 4.2 Movie App/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_4._2_Movie_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> films = new List<Movie>
+           {
+               new Movie("John Wick", "action"),
+               new Movie("John Wick 2", "action"),
+               new Movie("Man of Steel", "superHero"),
+               new Movie("End Game", "superHero"),
+               new Movie("Star Wars ep 4", "scifi"),
+               new Movie("Momento", "thiller"),
+               new Movie("The Shinning", "thiller"),
+               new Movie("DragonBall Super", "anime"),
+               new Movie("Pyscho Pass", "anime"),
+               new Movie("5th Element", "scifi"),
+           };
+
+            bool ohYeah = true;
+
+            while (ohYeah)
+            {
+                RunProgram(films);
+            }
+        }
+
+        private static void RunProgram(List<Movie> movies)
+        {
+         
+            
+            Console.WriteLine("What type of movie would you like to watch? \nAction \nSuperhero \nSciFi \nThiller \nAnime \nQuit \n");
+            string userInput = Console.ReadLine().ToLower();
+
+            foreach (Movie movie in movies)
+            {
+                if (movie.Genre == userInput)
+                {
+                    Console.WriteLine($"{ movie.Title}");
+                }
+            }
+            if (userInput == "quit")
+            {
+                Console.WriteLine("Have a great day");
+                System.Environment.Exit(0);
+            }
+            
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+    }
+}
